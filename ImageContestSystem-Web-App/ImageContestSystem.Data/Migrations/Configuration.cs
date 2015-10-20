f6c8778f693d3876e36b61ae5accefdfad7fe2af
Bare7a <bare7a@gmail.com>
2015-10-20 20:18:26 +0300

    Added DB Models

---
+++ ImageContestSystem-Web-App/ImageContestSystem.Data/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace ImageContestSystem.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ImageContestSystem.Data.ImageContestSystemContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "ImageContestSystem.Data.ImageContestSystemContext";
+        }
+
+        protected override void Seed(ImageContestSystem.Data.ImageContestSystemContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
