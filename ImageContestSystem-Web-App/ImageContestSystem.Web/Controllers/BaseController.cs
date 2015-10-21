801721c4e4c784e75022d461935059952ae5426b
bucaner0 <veselin.hitrov@gmail.com>
2015-10-21 13:56:20 +0300

    Add AutoMapper

--- ImageContestSystem-Web-App/ImageContestSystem.Web/Controllers/BaseController.cs
+++ ImageContestSystem-Web-App/ImageContestSystem.Web/Controllers/BaseController.cs
@@ -4,29 +4,35 @@
     using System.Linq;
     using System.Web.Mvc;
     using System.Web.Routing;
+
     using ImageContestSystem.Data.UnitOfWork;
     using ImageContestSystem.Models;
 
     public abstract class BaseController : Controller
     {
         private IImageContestSystemData data;
+
         private User userProfile;
 
-        public BaseController(IImageContestSystemData data)
+        protected BaseController(IImageContestSystemData data)
         {
             this.ContestData = data;
         }
 
-        public BaseController(IImageContestSystemData data, User userProfile)
+        protected BaseController(IImageContestSystemData data, User userProfile)
             : this(data)
         {
             this.UserProfile = userProfile;
         }
 
         protected IImageContestSystemData ContestData { get; private set; }
+
         protected User UserProfile { get; private set; }
 
-        protected override IAsyncResult BeginExecute(RequestContext requestContext, AsyncCallback callback, object state)
+        protected override IAsyncResult BeginExecute(
+            RequestContext requestContext, 
+            AsyncCallback callback, 
+            object state)
         {
             if (requestContext.HttpContext.User.Identity.IsAuthenticated)
             {
---
+++ ImageContestSystem-Web-App/ImageContestSystem.Web/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿namespace ImageContestSystem.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
+    using ImageContestSystem.Data.UnitOfWork;
+    using ImageContestSystem.Models;
+    using ImageContestSystem.Web.Models.ViewModels;
+
+    using Microsoft.AspNet.Identity;
+
+    public class UsersController : BaseController
+    {
+        public UsersController(IImageContestSystemData data)
+            : base(data)
+        {
+        }
+
+        public UsersController(IImageContestSystemData data, User userProfile)
+            : base(data, userProfile)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            var userId = this.User.Identity.GetUserId();
+            var user = this.ContestData.Users
+                .All()
+                .Where(x => x.Id == userId)
+                .Project()
+                .To<UserViewModel>()
+                .FirstOrDefault();
+
+            return this.View(user);
+        }
+    }
+}
