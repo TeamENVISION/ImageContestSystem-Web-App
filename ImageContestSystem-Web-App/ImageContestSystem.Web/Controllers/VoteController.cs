92b8c4bf2fcabf05db0cc184cc0b6bde28e37778
Bare7a <bare7a@gmail.com>
2015-10-24 23:17:53 +0300

    Added more actions to the Vote's controller and changed the Views alittle.

--- ImageContestSystem-Web-App/ImageContestSystem.Web/Controllers/VoteController.cs
+++ ImageContestSystem-Web-App/ImageContestSystem.Web/Controllers/VoteController.cs
@@ -22,33 +22,96 @@ namespace ImageContestSystem.Web.Controllers
         public VoteController(IImageContestSystemData data, User userProfile) : base(data, userProfile)
         {
         }
-        // GET: Available Contests For Voting
+        // GET: Available contests for voting
         public ActionResult Index()
         {
-            var conf = Mapper.Configuration;
-            conf.CreateMap<VoteViewModel, VoteViewModel>();
             string userId = this.User.Identity.GetUserId();
 
+            var conf = Mapper.Configuration;
+            conf.CreateMap<Contest, VoteViewModel>()
+                .ForMember(c => c.AvailableVotes, sr => sr
+                    .MapFrom(m => m.VotesCount - m.Pictures
+                    //.Where(p=> p.ContestId == m.ContestId)
+                    .SelectMany(v=>v.Votes)
+                    .Count(vo => vo.ParticipantId == userId && vo.Picture.ContestId == m.ContestId)));
+                    
+
             var contests = this.ContestData.Contest.All()
-                .Where(c => (c.Voters.Any(v=>v.Id == userId) || c.ContestStrategy.VotingStrategy == false) && c.HasEnded == false)
-                .Select(c => new VoteViewModel
-                {
-                    ContestId = c.ContestId,
-                    Title = c.Title,
-                    Description = c.Description,
-                    StartDate = c.StartDate,
-                    EndDate = c.EndDate,
-                    AvailableVotes = c.VotesCount - c.Participants.FirstOrDefault(p => p.Id == userId).Votes.Count(v=>v.Picture.ContestId == c.ContestId),
-                    VotesCount = c.VotesCount,
-                    HasEnded = c.HasEnded,
-                    OwnerUsername = c.Owner.UserName
-                })
+                .Where(c => (c.Voters.Any(v => v.Id == userId) || c.ContestStrategy.VotingStrategy == false) && c.HasEnded == false)
                 .Project()
                 .To<VoteViewModel>();
 
             return View(contests);
         }
 
+        //GET: All pictures from a single contest that you can vote to
+        public ActionResult Contest(int id)
+        {
+            string userId = this.User.Identity.GetUserId();
 
+            var contest = this.ContestData.Contest.Find(id);
+            if (contest == null)
+            {
+                return HttpNotFound();
+            }
+
+            bool canVote = contest.ContestStrategy.VotingStrategy == false || contest.Voters.Any(v => v.Id == userId);
+
+            if (!canVote)
+            {
+                return HttpNotFound();
+            }
+
+            var conf = Mapper.Configuration;
+            conf.CreateMap<Picture, PictureViewModel>();
+
+            var pictures = contest.Pictures.AsQueryable().Project().To<PictureViewModel>();
+            return View(pictures);
+        }
+
+
+        public ActionResult Vote(int id)
+        {
+            string userId = this.User.Identity.GetUserId();
+            var picture = this.ContestData.Pictures.Find(id);
+
+            if (picture == null)
+            {
+                return HttpNotFound();
+            }
+
+            bool canVote = picture.Contest.ContestStrategy.VotingStrategy == false ||
+                           picture.Contest.Participants.Any(p => p.Id == userId);
+
+            if (!canVote)
+            {
+                return HttpNotFound();
+            }
+
+            var vote = picture.Votes.FirstOrDefault(p => p.ParticipantId == userId && p.PictureId == id);
+
+            if (vote == null)
+            {
+                int availableVotes = picture.Contest.VotesCount - picture.Votes.Count(p => p.ParticipantId == userId);
+                if (availableVotes >= 1)
+                {
+                    Vote newVote = new Vote();
+                    newVote.ParticipantId = userId;
+                    newVote.PictureId = id;
+                    this.ContestData.Votes.Add(newVote);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+            else
+            {
+                this.ContestData.Votes.Delete(vote);
+            }
+            this.ContestData.SaveChanges();
+            return HttpNotFound();
+        }
     }
+
 }
