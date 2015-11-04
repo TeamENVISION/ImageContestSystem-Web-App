88bfbe13fccf27182617f3d78db9e1efd7cd8dc7
Ivan <zhostovivan@gmail.com>
2015-11-04 18:40:00 +0200

    Notification messages

---
+++ ImageContestSystem-Web-App/ImageContestSystem.Models/VoterInvitation.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImageContestSystem.Models
+{
+    public class VoterInvitation : Invitation
+    {
+        [Required]
+        public int ContestVotersId { get; set; }
+    }
+}
